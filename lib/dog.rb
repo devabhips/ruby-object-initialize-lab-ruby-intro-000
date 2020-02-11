5e91fa0b6a40e8008f1e4e18a4b7799805f62537
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-11 20:50:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 class Dog
-  def initialize(name, breed='Mutt')
+  def initialize(name, breed = 'Mutt')
     @name = name
     @breed = breed
   end
+
+  attr_writer :name
+  attr_reader :name
+  
+  attr_writer :breed
+  attr_reader :breed
 end
